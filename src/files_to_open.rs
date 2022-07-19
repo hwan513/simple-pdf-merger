f45f500244a6a19fcf1bedf1da7530a436ae67d8
hwan513 <wang.henryhan@gmail.com>
2022-07-19 22:53:49 +1200

    Implemented UI library to open files

--- src/files_to_open.rs
+++ src/files_to_open.rs
@@ -1,3 +1,7 @@
+use eframe::{
+    egui::{Button, CentralPanel, Label, Ui},
+    App,
+};
 use rfd::FileDialog;
 use std::path::PathBuf;
 
@@ -10,6 +14,7 @@ impl FilesToOpen {
         FilesToOpen { files: vec![] }
     }
 
+    // TODO See if need to be removed
     // fn add(&mut self, new_files: &mut Vec<PathBuf>) {
     //     self.files.append(new_files)
     // }
@@ -24,9 +29,31 @@ impl FilesToOpen {
         }
     }
 
-    pub fn display(&self) {
+    // TODO See if need to be removed
+    pub fn _display(&self) {
         for val in self.files.iter() {
             println!("{:?}", val);
         }
+    }
+
+    pub fn render_files(&self, ui: &mut Ui) {
+        for file in &self.files {
+            ui.add(Label::new(file.to_str().unwrap()));
+            ui.add_space(5.0);
+        }
+    }
+}
+
+impl App for FilesToOpen {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.heading("PDF Merger");
+            ui.add_space(10.0);
+            if ui.add(Button::new("Add Files")).clicked() {
+                self.open_file();
+            }
+            ui.add_space(10.0);
+            self.render_files(ui);
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod files_to_open;
 
+use eframe::{run_native, NativeOptions};
 use files_to_open::FilesToOpen;
 
 fn main() {
-    let mut files = FilesToOpen::new();
-    files.open_file();
-    files.display();
+    let native_options = NativeOptions::default();
+    run_native(
+        "PDF Merger UI",
+        native_options,
+        Box::new(|_cc| Box::new(FilesToOpen::new())),
+    );
 }
